7758d7adc06ea3396aefd9b91d6d86768fb9be2f
Kirill <djmonah48@gmail.com>
2022-09-24 12:45:51 +0300

    Корабль-игрок перемещается вправо при нажатие клавиши D (однократное)

--- first/alien_invasion.py
+++ first/alien_invasion.py
@@ -36,6 +36,10 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    # переместить корабль вправо.
+                    self.ship.rect.x += 1
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
         self.screen.fill(self.settings.bg_color)
