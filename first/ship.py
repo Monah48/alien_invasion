42980533be67f2fd788087f01771068bcf9b82a4
Kirill <djmonah48@gmail.com>
2022-09-28 20:25:32 +0300

    изменил скорость до "1"

--- first/ship.py
+++ first/ship.py
@@ -19,9 +19,9 @@ class Ship():
     def update(self):
         """Обновляет позицию корабля с учетом флага."""
         if self.moving_right:
-            self.rect.x += 0.5
+            self.rect.x += 1
         if self.moving_left:
-            self.rect.x -= 0.5
+            self.rect.x -= 1
 
     def blitme(self):
         """Рисует корабль в текущей позиции."""
