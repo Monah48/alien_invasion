0d04200060d6e4c52263c43ffd3c7a2ed82b0957
Monah48 <djmonah48@gmail.com>
2022-11-22 07:01:26 +0300

    Initialize the alien and create its initial position

--- first/alien.py
+++ first/alien.py
@@ -2,3 +2,9 @@ import pygame
 from pygame.sprite import Sprite
 class Alien(Sprite):
     """The class representing a single alien"""
+
+    def __init__(self, ai_game):
+        """Initialize the alien and create its initial position"""
+        super().__init__()
+        self.screen = ai_game.screen
+
